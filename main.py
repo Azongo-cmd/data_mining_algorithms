446af9544dc4a7927d62b8dc236953e6a215e5bd
ahzongo <abdelhassane.zongo@gmail.com>
2022-11-05 22:59:30 +0100

    implementation of  core functions

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import csv
-from unittest import result
 
-def get_data(filename):
+
+def get_data(filename:str) -> list:
     result = []
     count = 0
     with open(filename) as csvfile:
@@ -10,9 +10,28 @@ def get_data(filename):
             if count == 0:
                 count +=1
             else:
-                result.append(row[1].strip().split(","))
+                result.append(set(row[1].strip().split(",")))
     return  result
 
+def get_unique_items(dataset: list) -> list:
+    result = set()
+    for transaction in dataset:
+        for item in transaction:
+            result.add(item)
+    return result 
+
+def count_items(item: set, dataset: list) -> int:
+    total = 0
+    for transaction in dataset:
+        if item.issubset(transaction):
+            total +=1
+    return total
+
+def get_total_items(dataset:list) -> int:
+    total = 0
+    for transaction in dataset:
+        total += len(transaction)
+    return  total
 
 
-print(get_data('test.csv'))
+print(get_unique_items([['a', 'b'], ['a','d','e']]))
--- main_test.py
+++ main_test.py
@@ -1,14 +1,28 @@
 from pkgutil import get_data
 import unittest
-from main import get_data
+from main import *
 
 class  TestMain(unittest.TestCase):
 
     def  test_get_data(self):
         filename = 'test.csv'
-        result = [['a'], ['b','c'], ['d','c','e'], ['a','b','d','c'], ['e']]
+        result = [{'a'}, {'b','c'}, {'d','c','e'}, {'a','b','d','c'}, {'e'}]
         self.assertEqual(get_data(filename), result)
+    
 
+    def test_get_total_items(self):
+        dataset = [{'a'}, {'d','c','e'}, {'a','b','d','c'}, {'e'}]
+        self.assertEqual(get_total_items(dataset), 9)
+
+    def test_get_unique_items(self):
+        data = [{'a'}, {'d','c','e'}, {'a','b','d','c'}, {'e'}]
+        self.assertEqual(get_unique_items(data), {'a', 'b', 'c', 'd', 'e'})
+
+    def test_count_items(self):
+        data = data = [{'a'}, {'d','c','e'}, {'a','b','d','c'}, {'e'}]
+        self.assertEqual(count_items({'a'}, data), 2)
+        self.assertEqual(count_items({'d','c'}, data), 2)
+        self.assertEqual(count_items({'f'}, data), 0)
 
 
 
