1931c46a9dae95ba5b862274de700c79e6b9d95d
ahzongo <abdelhassane.zongo@gmail.com>
2022-11-10 11:53:04 +0100

    get_unique_items return a set instead of a list

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+import itertools
 
 
 def get_data(filename:str) -> list:
@@ -13,12 +14,12 @@ def get_data(filename:str) -> list:
                 result.append(set(row[1].strip().split(",")))
     return  result
 
-def get_unique_items(dataset: list) -> list:
+def get_unique_items(dataset: list) -> set:
     result = set()
     for transaction in dataset:
         for item in transaction:
             result.add(item)
-    return result 
+    return [set(item) for item in list(result)] 
 
 def count_items(item: set, dataset: list) -> int:
     total = 0
@@ -28,10 +29,59 @@ def count_items(item: set, dataset: list) -> int:
     return total
 
 def get_total_items(dataset:list) -> int:
-    total = 0
-    for transaction in dataset:
-        total += len(transaction)
-    return  total
+    return len(dataset)
 
+def frequent_itemset(previous_candidate: list, actual_canditate: list,min_support: int, dataset) -> list:
+    result = []
+    if previous_candidate == []:
+        for item in actual_canditate:
+            if count_items(item, dataset) >= min_support:
+                result.append(item)
+    else:
+        """
+        for item in actual_candidate
+         - get all subset of the item
+         - if all subset exist in the previous candidate, count and compare with the min_support
+         - else it can't be a frequent item
+        """
+        for item in actual_canditate:
+            item_subsets = get_subsets(item, len(previous_candidate[0]))
+            if is_all_subset_exist(previous_candidate, item_subsets):
+                if count_items(item, dataset) >= min_support:
+                    result.append(item)
+    return result
 
-print(get_unique_items([['a', 'b'], ['a','d','e']]))
+def get_subsets(item, subset_size):
+    return [set(subset) for subset in itertools.combinations(item, subset_size)]
+
+def is_all_subset_exist(candidate, subsets):
+    for subset in subsets:
+        if subset not in candidate:
+            return False
+    return True
+
+def generate_candidate(previous_candidate:list)-> list:
+    result = []
+    size =  len(previous_candidate[0]) + 1
+    for i in range(len(previous_candidate)):
+        set_union = set()
+        for j in range(i, len(previous_candidate)):
+            set_union = previous_candidate[i].union(previous_candidate[j])
+            for subset in itertools.combinations(set_union, size):
+                result.append(set(subset))
+
+    return result
+
+def apriori_principle(dataset, min_support) -> list:
+    result = []
+    previous_candidate = []
+    actual_candidate = get_unique_items(dataset) 
+    previous_candidate = frequent_itemset(previous_candidate,actual_candidate,min_support, dataset)
+    result.append(previous_candidate)
+    while previous_candidate != []:
+        actual_candidate = generate_candidate(previous_candidate)
+        previous_candidate = frequent_itemset(previous_candidate, actual_candidate, min_support, dataset)
+    return result
+#print(get_unique_items([['a', 'b'], ['a','d','e']]))
+data = get_data('test.csv')
+print(apriori_principle(data, 3))
--- main_test.py
+++ main_test.py
@@ -1,3 +1,4 @@
+from operator import sub
 from pkgutil import get_data
 import unittest
 from main import *
@@ -12,17 +13,29 @@ class  TestMain(unittest.TestCase):
 
     def test_get_total_items(self):
         dataset = [{'a'}, {'d','c','e'}, {'a','b','d','c'}, {'e'}]
-        self.assertEqual(get_total_items(dataset), 9)
+        self.assertEqual(get_total_items(dataset), 4)
 
     def test_get_unique_items(self):
         data = [{'a'}, {'d','c','e'}, {'a','b','d','c'}, {'e'}]
-        self.assertEqual(get_unique_items(data), {'a', 'b', 'c', 'd', 'e'})
+        self.assertEqual(get_unique_items(data), [{'e'}, {'b'}, {'d'}, {'a'}])
 
     def test_count_items(self):
         data = data = [{'a'}, {'d','c','e'}, {'a','b','d','c'}, {'e'}]
         self.assertEqual(count_items({'a'}, data), 2)
         self.assertEqual(count_items({'d','c'}, data), 2)
         self.assertEqual(count_items({'f'}, data), 0)
+    
+    def test_get_subset(self):
+        item = {'a','b', 'c'}
+        subsets = [{'a','b'}, {'a','c'}, {'b','c'}]
+        #self.ass(get_subsets(item, 2), subsets)
+    
+    def test_is_all_subset_exist(self):
+        subset1 = [{'a','b'}, {'a','c'}, {'b','c'}]
+        subset2 = [{'a','b'}, {'a','c'}, {'b','e'}]
+        candidate = [{'a','b'}, {'a','c'}, {'b','c'}, {'a','e'},{'c','e'}]
+        self.assertEqual(is_all_subset_exist(candidate,subset1), True)
+        self.assertEqual(is_all_subset_exist(candidate,subset2), False)
 
 
 
